8ce3156769cf199fbc8c5c4feac96dfbe817f27d
YousfiNacerEvo <yousfinacer270@gmail.com>
2025-06-22 10:51:06 +0100

    fixing production bugs

--- app/Login/update-password/page.js
+++ app/Login/update-password/page.js
@@ -1,6 +1,11 @@
 'use client';
 import UpdatePasswordForm from './UpdatePasswordForm';
+import { Suspense } from 'react';
 
 export default function UpdatePasswordPage() {
-  return <UpdatePasswordForm />;
+  return (
+    <Suspense fallback={<div>Chargement...</div>}>
+      <UpdatePasswordForm />
+    </Suspense>
+  );
 } 
