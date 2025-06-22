50f6a20fee49600543f8b2428afb89570e2b4af6
YousfiNacerEvo <yousfinacer270@gmail.com>
2025-06-22 11:17:16 +0100

    fixing reste password

--- app/Login/update-password/UpdatePasswordForm.js
+++ app/Login/update-password/UpdatePasswordForm.js
@@ -117,7 +117,7 @@ function UpdatePasswordFormContent() {
                 placeholder="New password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                disabled={!isCodeVerified || isLoading}
+                disabled={isLoading}
               />
             </div>
             <div>
@@ -131,7 +131,7 @@ function UpdatePasswordFormContent() {
                 placeholder="Confirm password"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
-                disabled={!isCodeVerified || isLoading}
+                disabled={isLoading}
               />
             </div>
           </div>
@@ -159,7 +159,7 @@ function UpdatePasswordFormContent() {
           <div>
             <button
               type="submit"
-              disabled={isLoading || !isCodeVerified}
+              disabled={isLoading}
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-indigo-400"
             >
               {isLoading ? 'Updating...' : 'Update password'}
