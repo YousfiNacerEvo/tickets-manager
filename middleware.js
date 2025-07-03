25139f2fe6321061e6952bf7972498c5ec6f15ca
YousfiNacerEvo <yousfinacer270@gmail.com>
2025-07-03 08:59:06 +0100

    fixing image bug

--- middleware.js
+++ middleware.js
@@ -38,13 +38,7 @@ export async function middleware(req) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     */
-    '/((?!_next/static|_next/image|favicon.ico|public/).*)',
+    // Exclure les fichiers statiques à la racine (ex: /logo.png, /robots.txt, etc.)
+    '/((?!_next/static|_next/image|favicon.ico|logo.png|public/).*)',
   ],
 }; 
